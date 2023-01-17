97621b28574663475a5afbfa98a19ed0af9b83fa
Jesús González <gonzalez.jesus@safetwice.com>
2023-01-17 19:33:02 +0100

    Add DebouncedStatus

---
+++ Lib.Test/Filters/DebouncedStatusTest.cs
@@ -0,0 +1,180 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2020 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using Utilities.Net.Filters;
+using Xunit;
+
+namespace Utilities.Net.Test.Filters
+{
+    public class DebouncedStatusTest
+    {
+        private bool m_activated;
+        private bool m_deactivated;
+
+        private void Check_OneActivation( DebouncedStatus filter, bool expectActive, bool expectActivation )
+        {
+            m_activated = false;
+            m_deactivated = false;
+
+            filter.UpdateStatus( true );
+
+            Assert.Equal( expectActive, filter.Active );
+            Assert.Equal( expectActivation, m_activated );
+            Assert.False( m_deactivated );
+        }
+
+        private void Check_OneDeactivation( DebouncedStatus filter, bool expectActive, bool expectDeactivation )
+        {
+            m_activated = false;
+            m_deactivated = false;
+
+            filter.UpdateStatus( false );
+
+            Assert.Equal( expectActive, filter.Active );
+            Assert.False( m_activated );
+            Assert.Equal( expectDeactivation, m_deactivated );
+        }
+
+        private void Check_Activation( DebouncedStatus filter )
+        {
+            Check_OneActivation( filter, false, false );
+
+            Check_OneActivation( filter, false, false );
+
+            Check_OneActivation( filter, true, true );
+        }
+
+        private void Check_Deactivation( DebouncedStatus filter )
+        {
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneDeactivation( filter, false, true );
+        }
+
+        [Fact]
+        public void Activation_Simple()
+        {
+            var filter = new DebouncedStatus( 3 );
+
+            filter.Activated += delegate
+            {
+                m_activated = true;
+            };
+
+            filter.Deactivated += delegate
+            {
+                m_deactivated = true;
+            };
+
+            Check_Activation( filter );
+        }
+
+        [Fact]
+        public void Activation_Complex()
+        {
+            var filter = new DebouncedStatus( 3 );
+
+            filter.Activated += delegate
+            {
+                m_activated = true;
+            };
+
+            filter.Deactivated += delegate
+            {
+                m_deactivated = true;
+            };
+
+            Check_OneActivation( filter, false, false );
+
+            Check_OneDeactivation( filter, false, false );
+
+            Check_OneActivation( filter, false, false );
+
+            Check_OneActivation( filter, false, false );
+
+            Check_OneDeactivation( filter, false, false );
+
+            Check_OneActivation( filter, false, false );
+
+            Check_OneActivation( filter, true, true );
+        }
+
+        [Fact]
+        public void Deactivation_Simple()
+        {
+            var filter = new DebouncedStatus( 3 );
+
+            filter.Activated += delegate
+            {
+                m_activated = true;
+            };
+
+            filter.Deactivated += delegate
+            {
+                m_deactivated = true;
+            };
+
+            Check_Activation( filter );
+
+            Check_Deactivation( filter );
+        }
+
+        [Fact]
+        public void Deactivation_Complex()
+        {
+            var filter = new DebouncedStatus( 3 );
+
+            filter.Activated += delegate
+            {
+                m_activated = true;
+            };
+
+            filter.Deactivated += delegate
+            {
+                m_deactivated = true;
+            };
+
+            Check_Activation( filter );
+
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneActivation( filter, true, false );
+
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneActivation( filter, true, false );
+
+            Check_OneDeactivation( filter, true, false );
+
+            Check_OneDeactivation( filter, false, true );
+        }
+
+        [Fact]
+        public void Multiple()
+        {
+            var filter = new DebouncedStatus( 3 );
+
+            filter.Activated += delegate
+            {
+                m_activated = true;
+            };
+
+            filter.Deactivated += delegate
+            {
+                m_deactivated = true;
+            };
+
+            for( int i = 0; i < 10; i++ )
+            {
+                Check_Activation( filter );
+
+                Check_Deactivation( filter );
+            }
+        }
+    }
+}
---
+++ Lib/Filters/DebouncedStatus.cs
@@ -0,0 +1,103 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2020 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+
+namespace Utilities.Net.Filters
+{
+    /// <summary>
+    /// Represents an status value that is debounced using a counter-based hysteresis filter.
+    /// </summary>
+    public class DebouncedStatus
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        /// <value>
+        /// Indicates the current filtered status.
+        /// </value>
+        public bool Active { get; private set; }
+
+        //===========================================================================
+        //                             PUBLIC EVENTS
+        //===========================================================================
+
+        /// <summary>
+        /// Event triggered when the status changes from not active to active.
+        /// </summary>
+        public event Action? Activated;
+
+        /// <summary>
+        /// Event triggered when the status changes from active to not active.
+        /// </summary>
+        public event Action? Deactivated;
+
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="activationLimit">Number of immediate activations / deactivations to change the status</param>
+        public DebouncedStatus( int activationLimit )
+        {
+            Active = false;
+            m_activationLimit = activationLimit;
+            m_count = 0;
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Updates the immediate (not filtered) status.
+        /// </summary>
+        /// <remarks>
+        /// The user must call this method to update the immediate status. 
+        /// When the accumulated activations reach the activation limit, the debounced status is set to active. 
+        /// Likewise, when the accumulated activations reach the deactivation limit, the debounced status is set to not active.
+        /// When the debounced status changes, the corresponding event is triggered.
+        /// </remarks>
+        /// <param name="active">Indicates if the immediate status is active</param>
+        public void UpdateStatus( bool active )
+        {
+            if( active )
+            {
+                if( m_count < m_activationLimit )
+                {
+                    m_count++;
+
+                    if( !Active && ( m_count == m_activationLimit ) )
+                    {
+                        Active = true;
+                        Activated?.Invoke();
+                    }
+                }
+            }
+            else
+            {
+                if( m_count > 0 )
+                {
+                    m_count--;
+
+                    if( Active && ( m_count == 0 ) )
+                    {
+                        Active = false;
+                        Deactivated?.Invoke();
+                    }
+                }
+            }
+        }
+
+        //===========================================================================
+        //                           PRIVATE ATTRIBUTES
+        //===========================================================================
+
+        private int m_activationLimit;
+        private int m_count;
+    }
+}
