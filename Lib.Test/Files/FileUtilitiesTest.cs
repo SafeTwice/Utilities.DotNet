3f14f2ed859e34ec3f70eb3bf2190d5db2a51ce9
Jesús González <gonzalez.jesus@safetwice.com>
2023-01-24 10:14:42 +0100

    Add FileUtilities

---
+++ Lib.Test/Files/FileUtilitiesTest.cs
@@ -0,0 +1,89 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2019-2023 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using System.IO;
+using Utilities.Net.Files;
+using Xunit;
+
+namespace Utilities.Net.Test.Files
+{
+    public class FileUtilitiesTest
+    {
+        [Fact]
+        public void EnsureDirectoryExists()
+        {
+            // Arrange
+
+            var testDirPath = Path.GetTempPath() + $"/{nameof( FileUtilitiesTest )}";
+
+            if( Directory.Exists( testDirPath ) )
+            {
+                Directory.Delete( testDirPath, true );
+            }
+
+            // Act
+
+            FileUtilities.EnsureDirectoryExists( testDirPath );
+
+            // Assert
+
+            Assert.True( Directory.Exists( testDirPath ) );
+
+            // Cleanup
+
+            Directory.Delete( testDirPath, true );
+        }
+
+        [Fact]
+        public void EnsureFilePathIsAvailable()
+        {
+            // Arrange
+
+            var testDirPath = Path.GetTempPath() + $"/{nameof( FileUtilitiesTest )}";
+            var tempFilePath = testDirPath + $"/{nameof( FileUtilitiesTest )}.txt";
+
+            if( Directory.Exists( testDirPath ) )
+            {
+                Directory.Delete( testDirPath, true );
+            }
+
+            // Act
+
+            FileUtilities.EnsureFilePathIsAvailable( tempFilePath );
+
+            // Assert
+
+            Assert.True( Directory.Exists( testDirPath ) );
+
+            // Cleanup
+
+            Directory.Delete( testDirPath, true );
+        }
+
+        [Fact]
+        public void EnsureFilePathIsAvailable_Exception()
+        {
+            // Act
+
+            var exception = Assert.Throws<ArgumentException>( () => FileUtilities.EnsureFilePathIsAvailable( "temp.txt" ) );
+
+            // Assert
+
+            Assert.Equal( "The specified path does not have directory information", exception.Message );
+        }
+
+        [Fact]
+        public void SanitizeFilename()
+        {
+            // Act
+
+            var result = FileUtilities.SanitizeFilename( "F|i\\l/e:name@$" );
+
+            // Assert
+
+            Assert.Equal( "Filename@$", result );
+        }
+    }
+}
---
+++ Lib/Files/FileUtilities.cs
@@ -0,0 +1,53 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2019-2023 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using System.IO;
+
+namespace Utilities.Net.Files
+{
+    /// <summary>
+    /// File utilities.
+    /// </summary>
+    public static class FileUtilities
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        public static void EnsureDirectoryExists( string dirPath )
+        {
+            if( !Directory.Exists( dirPath ) )
+            {
+                Directory.CreateDirectory( dirPath );
+            }
+        }
+
+        public static void EnsureFilePathIsAvailable( string path )
+        {
+            var dirName = Path.GetDirectoryName( path );
+            if( dirName?.Length > 0 )
+            {
+                EnsureDirectoryExists( dirName );
+            }
+            else
+            {
+                throw new ArgumentException( "The specified path does not have directory information" );
+            }
+
+        }
+
+        public static string SanitizeFilename( string filename )
+        {
+            string ret = filename;
+
+            foreach( var invalidChar in Path.GetInvalidFileNameChars() )
+            {
+                ret = ret.Replace( $"{invalidChar}", "" );
+            }
+
+            return ret;
+        }
+    }
+}
