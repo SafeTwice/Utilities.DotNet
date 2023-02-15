565783de4f645cf0c9f8f3e83178ca538b11383b
Jesús González <gonzalez.jesus@safetwice.com>
2023-02-15 17:36:55 +0100

    Add more utility methods to ArrayUtilities

--- Lib.Test/General/ArrayUtilitiesTest.cs
+++ Lib.Test/General/ArrayUtilitiesTest.cs
@@ -22,6 +22,52 @@ namespace Utilities.Net.Test
         }
 
         [Fact]
+        public void CreateArray_Generator_NoParam()
+        {
+            int x = 777;
+            var array = ArrayUtilities.CreateArray<int>( 78, () => x++ );
+
+            Assert.Equal( 78, array.Length );
+            for( int i = 0; i < array.Length; i++ )
+            {
+                Assert.Equal( 777 + i, array[ i ] );
+            }
+        }
+
+        [Fact]
+        public void CreateArray_Generator_WithParam()
+        {
+            var array = ArrayUtilities.CreateArray<int>( 34, ( n ) => ( n * 2 ) );
+
+            Assert.Equal( 34, array.Length );
+            for( int i = 0; i < array.Length; i++ )
+            {
+                Assert.Equal( i * 2, array[ i ] );
+            }
+        }
+
+        [Fact]
+        public void FillArray_Subarray()
+        {
+            var array = new long[ 75 ];
+
+            array.Fill( 543898876L, 10, 25 );
+
+            Assert.Equal( 75, array.Length );
+            for( int i = 0; i < array.Length; i++ )
+            {
+                if( ( i < 10 ) || ( i >= 35 ) )
+                {
+                    Assert.Equal( 0L, array[ i ] );
+                }
+                else
+                {
+                    Assert.Equal( 543898876L, array[ i ] );
+                }
+            }
+        }
+
+        [Fact]
         public void FillArray()
         {
             var array = new long[ 89 ];
@@ -33,6 +79,17 @@ namespace Utilities.Net.Test
             {
                 Assert.Equal( 543898876L, item );
             }
+        }
+
+        [Fact]
+        public void Subarray()
+        {
+            var inputArray = new int[] { 5, 234, -35, 4, 33 };
+            var expectedSubarray = new int[] { 234, -35, 4 };
+
+            var subarray = inputArray.Subarray( 1, 3 );
+
+            Assert.Equal( expectedSubarray, subarray );
         }
 
         [Fact]
@@ -50,6 +107,35 @@ namespace Utilities.Net.Test
             for( int i = 0; i < initialArray.Length; i++ )
             {
                 Assert.Equal( initialArray[ i ], clonedArray[ i ] );
+            }
+        }
+
+        [Fact]
+        public void ReversedSubarray()
+        {
+            var inputArray = new int[] { 5, 234, -35, 4, 33 };
+            var expectedSubarray = new int[] { 4, -35, 234 };
+
+            var subarray = inputArray.ReversedSubarray( 1, 3 );
+
+            Assert.Equal( expectedSubarray, subarray );
+        }
+
+        [Fact]
+        public void Reverse()
+        {
+            var initialArray = new DateTime[ 23 ];
+            for( int i = 0; i < initialArray.Length; i++ )
+            {
+                initialArray[ i ] = DateTime.Now.AddDays( i );
+            }
+
+            var reversedArray = initialArray.ReversedShallowClone();
+
+            Assert.Equal( initialArray.Length, reversedArray.Length );
+            for( int i = 0; i < initialArray.Length; i++ )
+            {
+                Assert.Equal( initialArray[ reversedArray.Length - 1 - i ], reversedArray[ i ] );
             }
         }
 
--- Lib/General/ArrayUtilities.cs
+++ Lib/General/ArrayUtilities.cs
@@ -36,6 +36,50 @@ namespace Utilities.Net
         }
 
         /// <summary>
+        /// Creates an array of the given <paramref name="size"/> filled with values provided by <paramref name="valueProvider"/>.
+        /// </summary>
+        /// <remarks>
+        /// The function <paramref name="valueProvider"/> is called once for each value of the array.
+        /// </remarks>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="size">Size of the array</param>
+        /// <param name="valueProvider">Function to provide the initial values for the array</param>
+        /// <returns>An array</returns>
+        public static T[] CreateArray<T>( uint size, Func<T> valueProvider )
+        {
+            T[] result = new T[ size ];
+
+            for( int i = 0; i < size; i++ )
+            {
+                result[ i ] = valueProvider();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Creates an array of the given <paramref name="size"/> filled with values provided by <paramref name="valueProvider"/>.
+        /// </summary>
+        /// <remarks>
+        /// The function <paramref name="valueProvider"/> is called once for each value of the array, passing the array index as parameter.
+        /// </remarks>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="size">Size of the array</param>
+        /// <param name="valueProvider">Function to provide the initial values for the array</param>
+        /// <returns>An array</returns>
+        public static T[] CreateArray<T>( uint size, Func<int,T> valueProvider )
+        {
+            T[] result = new T[ size ];
+
+            for( int i = 0; i < size; i++ )
+            {
+                result[ i ] = valueProvider( i );
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// Fills an <paramref name="array"/> with the given <paramref name="value"/>.
         /// </summary>
         /// <typeparam name="T">Type of the elements of the array</typeparam>
@@ -43,10 +87,41 @@ namespace Utilities.Net
         /// <param name="value">Value to be set</param>
         public static void Fill<T>( this T[] array, T value )
         {
-            for( int i = 0; i < array.Length; i++ )
+            array.Fill( value, 0, array.Length );
+        }
+
+        /// <summary>
+        /// Fills range of elements of an <paramref name="array"/> with the given <paramref name="value"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="array">An array</param>
+        /// <param name="value">Value to be set</param>
+        /// <param name="startIndex">Index at which filling begins</param>
+        /// <param name="count">Number of elements to fill</param>
+        public static void Fill<T>( this T[] array, T value, int startIndex, int count )
+        {
+            var endIndex = ( startIndex + count );
+            for( int i = startIndex; i < endIndex; i++ )
             {
                 array[ i ] = value;
             }
+        }
+
+        /// <summary>
+        /// Creates a new array filled with a range of elements from <paramref name="array"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="array">An array</param>
+        /// <param name="startIndex">Index at which element copying from the source array begins</param>
+        /// <param name="count">Number of elements to copy</param>
+        /// <returns>A shallow copy of the specified range of elements from the input array</returns>
+        public static T[] Subarray<T>( this T[] array, int startIndex, int count )
+        {
+            T[] subarray = new T[ count ];
+
+            Array.Copy( array, startIndex, subarray, 0, count );
+
+            return subarray;
         }
 
         /// <summary>
@@ -57,11 +132,35 @@ namespace Utilities.Net
         /// <returns>A shallow clone of the input array</returns>
         public static T[] ShallowClone<T>( this T[] array )
         {
-            T[] clonedArray = new T[ array.Length ];
+            return array.Subarray( 0, array.Length );
+        }
 
-            array.CopyTo( clonedArray, 0 );
+        /// <summary>
+        /// Creates a new array filled with a range of elements from <paramref name="array"/> in reverse order.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="array">An array</param>
+        /// <param name="startIndex">Index at which element copying from the source array begins</param>
+        /// <param name="count">Number of elements to copy</param>
+        /// <returns>A shallow copy of the specified range of elements from the input array in reverse order</returns>
+        public static T[] ReversedSubarray<T>( this T[] array, int startIndex, int count )
+        {
+            var subarray = array.Subarray( startIndex, count );
 
-            return clonedArray;
+            Array.Reverse( subarray );
+
+            return subarray;
+        }
+
+        /// <summary>
+        /// Creates a new array filled with the elements of <paramref name="array"/> in reverse order.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the array</typeparam>
+        /// <param name="array">An array</param>
+        /// <returns>A shallow copy of the input array in reverse order</returns>
+        public static T[] ReversedShallowClone<T>( this T[] array )
+        {
+            return array.ReversedSubarray( 0, array.Length );
         }
 
         /// <summary>
