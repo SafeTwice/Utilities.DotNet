ac3a900276fc2ca861943d572c55d5d007b48e72
Jesús González <gonzalez.jesus@safetwice.com>
2024-03-13 19:13:40 +0100

    Improve DoubleUtilities

--- Lib.Test/General/DoubleUtilitiesTest.cs
+++ Lib.Test/General/DoubleUtilitiesTest.cs
@@ -8,11 +8,67 @@ namespace Utilities.DotNet.Test
 {
     public class DoubleUtilitiesTest
     {
-        [Fact]
-        public void EqualsTest()
+        [Theory]
+        [InlineData( 15.00d, 15.19d, 0.20d, true )]
+        [InlineData( 15.00d, 15.21d, 0.21d, false )]
+        [InlineData( 268.0070d, 268.0061d, 0.001d, true )]
+        [InlineData( 268.0070d, 268.0059d, 0.001d, false )]
+        [InlineData( double.PositiveInfinity, double.PositiveInfinity, 0.000000000001d, true )]
+        [InlineData( double.PositiveInfinity, double.NegativeInfinity, 0.000000000001d, false )]
+        [InlineData( double.NegativeInfinity, double.PositiveInfinity, 0.000000000001d, false )]
+        [InlineData( 234.3d, double.NaN, 0.000000000001d, false )]
+        [InlineData( double.NaN, -2355.234d, 0.000000000001d, false )]
+        public void EqualsTest( double a, double b, double tolerance, bool result )
         {
-            Assert.True( DoubleUtilities.Equals( 15.0, 15.2, 0.21 ) );
-            Assert.False( DoubleUtilities.Equals( 268.006, 268.0071, 0.001 ) );
+            Assert.Equal( result, DoubleUtilities.Equals( a, b, tolerance ) );
+        }
+
+        [Theory]
+        [InlineData( 5467.245d, 5412.573d, 0.01d, true )]
+        [InlineData( 5467.245d, 5412.572d, 0.01d, false )]
+        [InlineData( 0.0003416d, 0.000345d, 0.01d, true )]
+        [InlineData( 0.0003415d, 0.000345d, 0.01d, false )]
+        [InlineData( double.PositiveInfinity, double.PositiveInfinity, 0.000000000001d, true )]
+        [InlineData( double.PositiveInfinity, double.NegativeInfinity, 0.000000000001d, false )]
+        [InlineData( double.NegativeInfinity, double.PositiveInfinity, 0.000000000001d, false )]
+        [InlineData( -234.3d, double.NaN, 0.000000000001d, false )]
+        [InlineData( double.NaN, 0.000008787324, 0.000000000001d, false )]
+        public void EqualsRelativeTest( double a, double b, double tolerance, bool result )
+        {
+            Assert.Equal( result, DoubleUtilities.EqualsRelative( a, b, tolerance ) );
+        }
+
+        [Theory]
+        [InlineData( 15.00d, 15.19d, 0.20d, 0 )]
+        [InlineData( 15.00d, 15.21d, 0.21d, -1 )]
+        [InlineData( 268.0070d, 268.0061d, 0.001d, 0 )]
+        [InlineData( 268.0070d, 268.0059d, 0.001d, 1 )]
+        [InlineData( double.PositiveInfinity, double.PositiveInfinity, 0.000000000001d, 0 )]
+        [InlineData( double.PositiveInfinity, double.NegativeInfinity, 0.000000000001d, 1 )]
+        [InlineData( double.NegativeInfinity, double.PositiveInfinity, 0.000000000001d, -1 )]
+        [InlineData( double.NaN, 0.000008787324, 0.000000000001d, -1 )]
+        [InlineData( -234.3d, double.NaN, 0.000000000001d, 1 )]
+        [InlineData( 234.3d, double.NaN, 0.000000000001d, 1 )]
+        public void CompareToTest( double a, double b, double tolerance, int result )
+        {
+            Assert.Equal( result, DoubleUtilities.CompareTo( a, b, tolerance ) );
+        }
+
+        [Theory]
+        [InlineData( 5467.245d, 5412.573d, 0.01d, 0 )]
+        [InlineData( 5467.245d, 5412.572d, 0.01d, 1 )]
+        [InlineData( -0.0003416d, -0.000345d, 0.01d, 0 )]
+        [InlineData( -0.0003415d, -0.000345d, 0.01d, 1 )]
+        [InlineData( 0.0003415d, 0.000345d, 0.01d, -1 )]
+        [InlineData( double.PositiveInfinity, double.PositiveInfinity, 0.000000000001d, 0 )]
+        [InlineData( double.PositiveInfinity, double.NegativeInfinity, 0.000000000001d, 1 )]
+        [InlineData( double.NegativeInfinity, double.PositiveInfinity, 0.000000000001d, -1 )]
+        [InlineData( double.NaN, 0.000008787324, 0.000000000001d, -1 )]
+        [InlineData( -234.3d, double.NaN, 0.000000000001d, 1 )]
+        [InlineData( 234.3d, double.NaN, 0.000000000001d, 1 )]
+        public void CompareRelativeToTest( double a, double b, double tolerance, int result )
+        {
+            Assert.Equal( result, DoubleUtilities.CompareRelativeTo( a, b, tolerance ) );
         }
     }
 }
--- Lib/General/DoubleUtilities.cs
+++ Lib/General/DoubleUtilities.cs
@@ -16,15 +16,98 @@ namespace Utilities.DotNet
         //===========================================================================
 
         /// <summary>
-        /// Compares two doubles.
+        /// Compares two doubles using absolute difference comparison.
         /// </summary>
-        /// <param name="a">A double</param>
-        /// <param name="b">Another double</param>
-        /// <param name="precision">Comparison precision</param>
-        /// <returns><c>true</c> if <paramref name="a"/> and <paramref name="b"/> have an absolute difference lower than <paramref name="precision"/></returns>
-        public static bool Equals( double a, double b, double precision )
+        /// <param name="a">A double.</param>
+        /// <param name="b">Another double.</param>
+        /// <param name="tolerance">Maximum allowed absolute difference.</param>
+        /// <returns><c>true</c> if <paramref name="a"/> and <paramref name="b"/> have an absolute difference lower than <paramref name="tolerance"/>.</returns>
+        public static bool Equals( this double a, double b, double tolerance )
         {
-            return ( Math.Abs( a - b ) < precision );
+            if( a.Equals( b ) )
+            {
+                return true;
+            }
+
+            var absDiff = Math.Abs( a - b );
+
+            return !double.IsNaN( absDiff ) && ( absDiff < tolerance );
+        }
+
+        /// <summary>
+        /// Compares two doubles using relative difference comparison.
+        /// </summary>
+        /// <param name="a">A double.</param>
+        /// <param name="b">Another double.</param>
+        /// <param name="tolerance">Maximum allowed relative difference.</param>
+        /// <returns><c>true</c> if <paramref name="a"/> and <paramref name="b"/> have a relative difference lower than <paramref name="tolerance"/>.</returns>
+        public static bool EqualsRelative( this double a, double b, double tolerance )
+        {
+            if( a.Equals( b ) )
+            {
+                return true;
+            }
+
+            var absDiff = Math.Abs( a - b );
+            var absMax = Math.Max( Math.Abs( a ), Math.Abs( b ) );
+
+            return !double.IsNaN( absMax ) && ( ( absDiff / absMax ) < tolerance );
+        }
+
+        /// <summary>
+        /// Compares two doubles using absolute difference comparison and returns an indication of their relative values.
+        /// </summary>
+        /// <remarks>
+        /// The returned value is:
+        /// <list type="bullet">
+        /// <item>Zero: when <paramref name="a"/> is equal to <paramref name="b"/>.</item>
+        /// <item>Less than zero: when <paramref name="a"/> is less than <paramref name="b"/>.</item>
+        /// <item>Greater than zero: when <paramref name="a"/> is greater than <paramref name="b"/>.</item>
+        /// </list>
+        /// </remarks>
+        /// <param name="a">A double.</param>
+        /// <param name="b">Another double.</param>
+        /// <param name="tolerance">Maximum allowed absolute difference.</param>
+        /// <returns>A signed number indicating the relative values of the compared numbers.
+        /// </returns>
+        public static int CompareTo( this double a, double b, double tolerance )
+        {
+            if( a.Equals( b, tolerance ) )
+            {
+                return 0;
+            }
+            else
+            {
+                return a.CompareTo( b );
+            }
+        }
+
+        /// <summary>
+        /// Compares two doubles using relative difference comparison and returns an indication of their relative values.
+        /// </summary>
+        /// <remarks>
+        /// The returned value is:
+        /// <list type="bullet">
+        /// <item>Zero: when <paramref name="a"/> is equal to <paramref name="b"/>.</item>
+        /// <item>Less than zero: when <paramref name="a"/> is less than <paramref name="b"/>.</item>
+        /// <item>Greater than zero: when <paramref name="a"/> is greater than <paramref name="b"/>.</item>
+        /// </list>
+        /// </remarks>
+        /// <param name="a">A double.</param>
+        /// <param name="b">Another double.</param>
+        /// <param name="tolerance">Maximum allowed absolute difference.</param>
+        /// <returns>A signed number indicating the relative values of the compared numbers.
+        /// </returns>
+        public static int CompareRelativeTo( this double a, double b, double tolerance )
+        {
+            if( a.EqualsRelative( b, tolerance ) )
+            {
+                return 0;
+            }
+            else
+            {
+                return a.CompareTo( b );
+            }
         }
     }
 }
