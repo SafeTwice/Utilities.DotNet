a4bb3ffe7f1f84502a2b2f6d3d60a6fb503f46ef
Jesús González <gonzalez.jesus@safetwice.com>
2024-04-11 19:15:31 +0200

    Add move ArrayUtilities.ToHexString to EnumerableUtilities, add ArrayUtilities.ParseHexString

--- Lib/General/ArrayUtilities.cs
+++ Lib/General/ArrayUtilities.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Text;
+using static Utilities.Net.I18N.LibraryLocalizer;
 
 namespace Utilities.Net
 {
@@ -19,9 +20,9 @@ namespace Utilities.Net
         /// <summary>
         /// Creates an array of the given <paramref name="size"/> filled with <paramref name="initValue"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="size">Size of the array</param>
-        /// <param name="initValue">Initial value for the elements</param>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="size">Size of the array.</param>
+        /// <param name="initValue">Initial value for the elements.</param>
         /// <returns>An array</returns>
         public static T[] CreateArray<T>( uint size, T initValue )
         {
@@ -41,9 +42,9 @@ namespace Utilities.Net
         /// <remarks>
         /// The function <paramref name="valueProvider"/> is called once for each value of the array.
         /// </remarks>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="size">Size of the array</param>
-        /// <param name="valueProvider">Function to provide the initial values for the array</param>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="size">Size of the array.</param>
+        /// <param name="valueProvider">Function to provide the initial values for the array.</param>
         /// <returns>An array</returns>
         public static T[] CreateArray<T>( uint size, Func<T> valueProvider )
         {
@@ -63,9 +64,9 @@ namespace Utilities.Net
         /// <remarks>
         /// The function <paramref name="valueProvider"/> is called once for each value of the array, passing the array index as parameter.
         /// </remarks>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="size">Size of the array</param>
-        /// <param name="valueProvider">Function to provide the initial values for the array</param>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="size">Size of the array.</param>
+        /// <param name="valueProvider">Function to provide the initial values for the array.</param>
         /// <returns>An array</returns>
         public static T[] CreateArray<T>( uint size, Func<int,T> valueProvider )
         {
@@ -82,9 +83,9 @@ namespace Utilities.Net
         /// <summary>
         /// Fills an <paramref name="array"/> with the given <paramref name="value"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <param name="value">Value to be set</param>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="array">An array.</param>
+        /// <param name="value">Value to be set.</param>
         public static void Fill<T>( this T[] array, T value )
         {
             array.Fill( value, 0, array.Length );
@@ -93,11 +94,11 @@ namespace Utilities.Net
         /// <summary>
         /// Fills range of elements of an <paramref name="array"/> with the given <paramref name="value"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <param name="value">Value to be set</param>
-        /// <param name="startIndex">Index at which filling begins</param>
-        /// <param name="count">Number of elements to fill</param>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="array">An array.</param>
+        /// <param name="value">Value to be set.</param>
+        /// <param name="startIndex">Index at which filling begins.</param>
+        /// <param name="count">Number of elements to fill.</param>
         public static void Fill<T>( this T[] array, T value, int startIndex, int count )
         {
             var endIndex = ( startIndex + count );
@@ -110,11 +111,11 @@ namespace Utilities.Net
         /// <summary>
         /// Creates a new array filled with a range of elements from <paramref name="array"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <param name="startIndex">Index at which element copying from the source array begins</param>
-        /// <param name="count">Number of elements to copy</param>
-        /// <returns>A shallow copy of the specified range of elements from the input array</returns>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="array">An array.</param>
+        /// <param name="startIndex">Index at which element copying from the source array begins.</param>
+        /// <param name="count">Number of elements to copy.</param>
+        /// <returns>A shallow copy of the specified range of elements from the input array.</returns>
         public static T[] Subarray<T>( this T[] array, int startIndex, int count )
         {
             T[] subarray = new T[ count ];
@@ -127,9 +128,9 @@ namespace Utilities.Net
         /// <summary>
         /// Creates a new array filled with the elements of <paramref name="array"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <returns>A shallow clone of the input array</returns>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="array">An array.</param>
+        /// <returns>A shallow clone of the input array.</returns>
         public static T[] ShallowClone<T>( this T[] array )
         {
             return array.Subarray( 0, array.Length );
@@ -139,10 +140,10 @@ namespace Utilities.Net
         /// Creates a new array filled with a range of elements from <paramref name="array"/> in reverse order.
         /// </summary>
         /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <param name="startIndex">Index at which element copying from the source array begins</param>
-        /// <param name="count">Number of elements to copy</param>
-        /// <returns>A shallow copy of the specified range of elements from the input array in reverse order</returns>
+        /// <param name="array">An array.</param>
+        /// <param name="startIndex">Index at which element copying from the source array begins.</param>
+        /// <param name="count">Number of elements to copy.</param>
+        /// <returns>A shallow copy of the specified range of elements from the input array in reverse orde.r</returns>
         public static T[] ReversedSubarray<T>( this T[] array, int startIndex, int count )
         {
             var subarray = array.Subarray( startIndex, count );
@@ -155,9 +156,9 @@ namespace Utilities.Net
         /// <summary>
         /// Creates a new array filled with the elements of <paramref name="array"/> in reverse order.
         /// </summary>
-        /// <typeparam name="T">Type of the elements of the array</typeparam>
-        /// <param name="array">An array</param>
-        /// <returns>A shallow copy of the input array in reverse order</returns>
+        /// <typeparam name="T">Type of the elements of the array.</typeparam>
+        /// <param name="array">An array.</param>
+        /// <returns>A shallow copy of the input array in reverse order.</returns>
         public static T[] ReversedShallowClone<T>( this T[] array )
         {
             return array.ReversedSubarray( 0, array.Length );
@@ -166,8 +167,8 @@ namespace Utilities.Net
         /// <summary>
         /// Decodes an ASCII string from a byte array.
         /// </summary>
-        /// <param name="array">Array to decode the string from</param>
-        /// <returns>An string decoded from its ASCII representation in the array</returns>
+        /// <param name="array">Array to decode the string from.</param>
+        /// <returns>An string decoded from its ASCII representation in the array.</returns>
         public static string DecodeASCII( this byte[] array )
         {
             return array.DecodeASCII( 0, array.Length );
@@ -176,10 +177,10 @@ namespace Utilities.Net
         /// <summary>
         /// Decodes an ASCII string from a byte array.
         /// </summary>
-        /// <param name="array">Array to decode the string from</param>
-        /// <param name="offset">Offset to start decoding from</param>
-        /// <param name="length">Length to decode</param>
-        /// <returns>An string decoded from its ASCII representation in the array</returns>
+        /// <param name="array">Array to decode the string from.</param>
+        /// <param name="offset">Offset to start decoding from.</param>
+        /// <param name="length">Length to decode.</param>
+        /// <returns>An string decoded from its ASCII representation in the array.</returns>
         public static string DecodeASCII( this byte[] array, int offset, int length )
         {
             var firstNULindex = Array.IndexOf( array, (byte) 0, offset, length );
@@ -193,42 +194,39 @@ namespace Utilities.Net
         /// <summary>
         /// Encodes the ASCII representation of a string into a byte array.
         /// </summary>
-        /// <param name="str">String to encode</param>
-        /// <returns>A byte array with the ASCII representation of the string</returns>
+        /// <param name="str">String to encode.</param>
+        /// <returns>A byte array with the ASCII representation of the string.</returns>
         public static byte[] EncodeASCII( this string str )
         {
             return Encoding.ASCII.GetBytes( str );
         }
 
         /// <summary>
-        /// Converts a byte array to its corresponding hexadecimal representation.
+        /// Converts a string containing the hexadecimal representation of a byte array to its corresponding byte array.
         /// </summary>
-        /// <param name="value">A byte array</param>
-        /// <param name="separator">An optional string to include between byte's representations</param>
-        /// <returns>String containing the hexadecimal representation of the input array</returns>
-        public static string ToHexString( this byte[] value, string separator = "" )
+        /// <param name="hexString">String containing the hexadecimal representation of a byte array.</param>
+        /// <returns>The byte array that corresponds to the input string.</returns>
+        public static byte[] ParseHexString( string hexString )
         {
-            if( value.Length == 0 )
+            if( ( hexString.Length % 2 ) != 0 )
             {
-                return string.Empty;
+                throw new ArgumentException( Localize( $"The value '{hexString}' is not a valid hexadecimal string (wrong length)" ) );
             }
 
-            StringBuilder result = new StringBuilder( ( value.Length * 2 ) + ( ( value.Length - 1 ) * separator.Length ) );
-            bool addSeparator = false;
-
-            foreach( byte b in value )
+            var result = new byte[ hexString.Length / 2 ];
+            for( int i = 0; i < ( hexString.Length / 2 ); i++ )
             {
-                if( addSeparator )
+                try
                 {
-                    result.Append( separator );
+                    result[ i ] = Convert.ToByte( hexString.Substring( i * 2, 2 ), 16 );
                 }
-
-                result.AppendFormat( "{0:X2}", b );
-
-                addSeparator = true;
+                catch( Exception e )
+                {
+                    throw new ArgumentException( Localize( $"The value '{hexString}' is not a valid hexadecimal string (wrong format)" ), e );
+                }
             }
 
-            return result.ToString();
+            return result;
         }
     }
 }
