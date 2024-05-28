3d5463bb0306a41fc438484d1dc963de4c33e2ed
Jesús González <gonzalez.jesus@safetwice.com>
2024-05-28 14:03:41 +0200

    Add ListEx

---
+++ Collections.Test/ListExTest.cs
@@ -0,0 +1,93 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Collections;
+using System.Collections.Generic;
+using Utilities.DotNet.Collections;
+using Xunit;
+
+#pragma warning disable xUnit2013 // Do not use equality check to check for collection size.
+
+namespace Utilities.DotNet.Test.Collections
+{
+    public class ListExTest
+    {
+        [Fact]
+        public void Constructor_Default()
+        {
+            var list = new ListEx<int>();
+
+            Assert.Equal( 0, list.Count );
+            Assert.False( ( (ICollection<int>) list ).IsReadOnly );
+            Assert.False( ( (IList) list ).IsReadOnly );
+            Assert.False( ( (IList) list ).IsFixedSize );
+            Assert.NotNull( ( (IList) list ).SyncRoot );
+            Assert.False( ( (IList) list ).IsSynchronized );
+        }
+
+        [Fact]
+        public void Constructor_InitializationList()
+        {
+            ListEx<int> list = new ListEx<int>( new[] { 5, 8, 2 } );
+
+            Assert.Equal( new[] { 5, 8, 2 }, list );
+            Assert.False( ( (IList) list ).IsReadOnly );
+            Assert.False( ( (IList) list ).IsFixedSize );
+            Assert.NotNull( ( (IList) list ).SyncRoot );
+            Assert.False( ( (IList) list ).IsSynchronized );
+        }
+
+        [Fact]
+        public void Constructor_Capacity()
+        {
+            var list = new ListEx<int>( 10 );
+
+            Assert.Equal( 0, list.Count );
+            Assert.Equal( 10, list.Capacity );
+            Assert.False( ( (ICollection<int>) list ).IsReadOnly );
+            Assert.False( ( (IList) list ).IsReadOnly );
+            Assert.False( ( (IList) list ).IsFixedSize );
+            Assert.NotNull( ( (IList) list ).SyncRoot );
+            Assert.False( ( (IList) list ).IsSynchronized );
+        }
+
+        [Fact]
+        public void GetRange()
+        {
+            ListEx<int> list = new ListEx<int>( new[] { 5, 8, 2 } );
+
+            var range1 = list.GetRange( 1, 2 );
+
+            Assert.Equal( new[] { 8, 2 }, range1 );
+
+            var range2 = list.GetRange( 0, 1 );
+
+            Assert.Equal( new[] { 5 }, range2 );
+        }
+
+        [Fact]
+        public void Slice()
+        {
+            ListEx<int> list = new ListEx<int>( new[] { 5, 8, 2 } );
+
+            var range1 = list.Slice( 1, 2 );
+
+            Assert.Equal( new[] { 8, 2 }, range1 );
+
+            var range2 = list.Slice( 0, 1 );
+
+            Assert.Equal( new[] { 5 }, range2 );
+        }
+
+        [Fact]
+        public void RemoveRange()
+        {
+            ListEx<int> list = new ListEx<int>( new[] { 5, 8, 2, 9 } );
+
+            list.RemoveRange( new[] { 8, 2 } );
+
+            Assert.Equal( new[] { 5, 9 }, list );
+        }
+    }
+}
---
+++ Collections/ListEx.cs
@@ -0,0 +1,75 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Collections.Generic;
+
+namespace Utilities.DotNet.Collections
+{
+    /// <summary>
+    /// Extension of <see cref="List{T}"/> that implements <see cref="IListEx{T}"/>.
+    /// </summary>
+    public class ListEx<T> : List<T>, IListEx<T>
+    {
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Initializes a new instance that is empty and has the default initial capacity.
+        /// </summary>
+        public ListEx()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance that contains elements copied from the specified collection and
+        /// has sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="collection">Collection whose elements are copied to the new list.</param>
+        public ListEx( IEnumerable<T> collection ) : base( collection )
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance that is empty and has the specified initial capacity.
+        /// </summary>
+        /// <param name="capacity">Number of elements that the new list can initially store.</param>
+        public ListEx( int capacity ) : base( capacity )
+        {
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <inheritdoc/>
+        public new IListEx<T> GetRange( int index, int count )
+        {
+            return new ListEx<T>( base.GetRange( index, count ) );
+        }
+
+#if NET8_0_OR_GREATER
+        /// <inheritdoc/>
+        public new IListEx<T> Slice( int start, int length )
+        {
+            return GetRange( start, length );
+        }
+#else
+        /// <inheritdoc/>
+        public IListEx<T> Slice( int start, int length )
+        {
+            return GetRange( start, length );
+        }
+#endif
+
+        /// <inheritdoc/>
+        public void RemoveRange( IEnumerable<T> collection )
+        {
+            foreach( var item in collection )
+            {
+                Remove( item );
+            }
+        }
+    }
+}
