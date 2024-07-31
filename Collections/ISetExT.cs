c72b0e531c5a007a9476e442251cfef86397a0ce
Jesús González <gonzalez.jesus@safetwice.com>
2024-07-31 10:12:35 +0200

    Fix ambiguity errors

--- Collections/ISetExT.cs
+++ Collections/ISetExT.cs
@@ -12,5 +12,7 @@ namespace Utilities.DotNet.Collections
     /// <typeparam name="T">Type of the items in the set.</typeparam>
     public interface ISetEx<T> : ISet<T>, ICollectionEx<T>
     {
+        /// <inheritdoc cref="ISet{T}.Add(T)"/>
+        new bool Add( T item ); // Needed to disambiguate between ISet<> and ICollectionEx<> and avoid error CS0121
     }
 }
