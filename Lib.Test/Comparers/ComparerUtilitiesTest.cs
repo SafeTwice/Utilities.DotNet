b195ab90c197a603f4862fed418a0775195e18e2
Jesús González <gonzalez.jesus@safetwice.com>
2024-10-18 15:25:33 +0200

    Add ComparerUtilities

---
+++ Lib.Test/Comparers/ComparerUtilitiesTest.cs
@@ -0,0 +1,91 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2023 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using Utilities.DotNet.Comparers;
+using Xunit;
+
+namespace Utilities.DotNet.Test.Comparers
+{
+    public class ComparerUtilitiesTest
+    {
+        private class TestClass
+        {
+            public int IntValue { get; set; }
+
+            public string StringValue { get; set; } = string.Empty;
+        }
+
+        [Fact]
+        public void CreateComparer_Simple()
+        {
+            // Arrange
+
+            var testObject1 = new TestClass { IntValue = 1 };
+            var testObject2 = new TestClass { IntValue = 2 };
+            var testObject3 = new TestClass { IntValue = 3 };
+            var testObject4 = new TestClass { IntValue = 1 };
+
+            // Act
+
+            var comparer = ComparerUtilities.CreateComparer<TestClass?>( o => new object[] { o.IntValue } );
+
+            // Assert
+
+            Assert.Equal( 0, comparer.Compare( testObject1, testObject1 ) );
+            Assert.Equal( 0, comparer.Compare( testObject1, testObject4 ) );
+            Assert.Equal( -1, comparer.Compare( testObject1, testObject2 ) );
+            Assert.Equal( 1, comparer.Compare( testObject3, testObject2 ) );
+
+            Assert.Equal( 0, comparer.Compare( null, null ) );
+            Assert.Equal( 1, comparer.Compare( testObject1, null ) );
+            Assert.Equal( -1, comparer.Compare( null, testObject1 ) );
+        }
+
+        [Fact]
+        public void CreateComparer_Complex()
+        {
+            // Arrange
+
+            var testObject1 = new TestClass { IntValue = 1, StringValue = "Uno" };
+            var testObject2 = new TestClass { IntValue = 2, StringValue = "Dos" };
+            var testObject3 = new TestClass { IntValue = 3, StringValue = "Tres" };
+            var testObject4 = new TestClass { IntValue = 1, StringValue = "Uno" };
+            var testObject5 = new TestClass { IntValue = 1, StringValue = "UnoYMedio" };
+
+            // Act
+
+            var comparer = ComparerUtilities.CreateComparer<TestClass>( o => new object[] { o.IntValue, o.StringValue } );
+
+            // Assert
+
+            Assert.Equal( 0, comparer.Compare( testObject1, testObject1 ) );
+            Assert.Equal( -1, comparer.Compare( testObject1, testObject2 ) );
+            Assert.Equal( 1, comparer.Compare( testObject3, testObject2 ) );
+            Assert.Equal( 0, comparer.Compare( testObject1, testObject4 ) );
+            Assert.Equal( -1, comparer.Compare( testObject1, testObject5 ) );
+        }
+
+        [Fact]
+        public void CreateComparer_DifferentDataLengths()
+        {
+            // Arrange
+
+            var testObject1 = new TestClass { IntValue = 1, StringValue = "Uno" };
+            var testObject2 = new TestClass { IntValue = 2 };
+            var testObject3 = new TestClass { IntValue = 1 };
+
+            // Act
+
+            var comparer = ComparerUtilities.CreateComparer<TestClass>( o => ( o.StringValue.Length > 0 ) ? new object[] { o.IntValue, o.StringValue } :
+                                                                                                            new object[] { o.IntValue } );
+
+            // Assert
+
+            Assert.Equal( 0, comparer.Compare( testObject1, testObject1 ) );
+            Assert.Equal( -1, comparer.Compare( testObject1, testObject2 ) );
+            Assert.Equal( 1, comparer.Compare( testObject1, testObject3 ) );
+            Assert.Equal( -1, comparer.Compare( testObject3, testObject1 ) );
+        }
+    }
+}
---
+++ Lib/Comparers/ComparerUtilities.cs
@@ -0,0 +1,95 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utilities.DotNet.Comparers
+{
+    /// <summary>
+    /// {Enter brief class description here...}
+    /// </summary>
+    public static class ComparerUtilities
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Creates a comparer based on the provided data collector function.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="dataCollector">The function that collects the data to be compared.</param>
+        /// <returns>A comparer for the specified type.</returns>
+        public static IComparer<T> CreateComparer<T>( Func<T, IEnumerable> dataCollector )
+        {
+            return new DataComparer<T>( dataCollector );
+        }
+
+        //===========================================================================
+        //                          PRIVATE NESTED TYPES
+        //===========================================================================
+
+        private class DataComparer<T> : IComparer<T>
+        {
+            public DataComparer( Func<T, IEnumerable> dataCollector )
+            {
+                m_dataCollector = dataCollector;
+            }
+
+            public int Compare( T? x, T? y )
+            {
+                if( x == null && y == null )
+                {
+                    return 0;
+                }
+                else if( x == null )
+                {
+                    return -1;
+                }
+                else if( y == null )
+                {
+                    return 1;
+                }
+
+                var xData = m_dataCollector( x );
+                var yData = m_dataCollector( y );
+
+                var xEnumerator = xData.GetEnumerator();
+                var yEnumerator = yData.GetEnumerator();
+
+                while( true )
+                {
+                    var xHasNext = xEnumerator.MoveNext();
+                    var yHasNext = yEnumerator.MoveNext();
+
+                    if( !xHasNext && !yHasNext )
+                    {
+                        return 0;
+                    }
+
+                    if( !xHasNext )
+                    {
+                        return -1;
+                    }
+
+                    if( !yHasNext )
+                    {
+                        return 1;
+                    }
+
+                    var comparison = Comparer.Default.Compare( xEnumerator.Current, yEnumerator.Current );
+
+                    if( comparison != 0 )
+                    {
+                        return comparison;
+                    }
+                }
+            }
+
+            private readonly Func<T, IEnumerable> m_dataCollector;
+        }
+    }
+}
