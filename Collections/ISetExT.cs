d90ae665a83bfd6805027747eadfc6acb4790eab
Jesús González <gonzalez.jesus@safetwice.com>
2024-11-06 20:11:19 +0100

    Declare ISetEx<T> to implement IReadOnlySetEx<T>

--- Collections/ISetExT.cs
+++ Collections/ISetExT.cs
@@ -10,7 +10,7 @@ namespace Utilities.DotNet.Collections
     /// Extension of the <see cref="ISet{T}"/> interface that provides additional methods.
     /// </summary>
     /// <typeparam name="T">Type of the items in the set.</typeparam>
-    public interface ISetEx<T> : ISet<T>, ICollectionEx<T>
+    public interface ISetEx<T> : ISet<T>, ICollectionEx<T>, IReadOnlySetEx<T>
     {
         /// <inheritdoc cref="ISet{T}.Add(T)"/>
         new bool Add( T item ); // Needed to disambiguate between ISet<> and ICollectionEx<> and avoid error CS0121
