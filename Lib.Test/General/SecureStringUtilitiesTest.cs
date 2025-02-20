349b7cfedb635e3962b2c804008a4b89a0516733
Jesús González <gonzalez.jesus@safetwice.com>
2025-02-20 16:43:02 +0100

    Add SecureString utilities

---
+++ Lib.Test/General/SecureStringUtilitiesTest.cs
@@ -0,0 +1,59 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2025 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Security;
+using Xunit;
+
+namespace Utilities.DotNet.Test
+{
+    public class SecureStringUtilitiesTest
+    {
+        [Fact]
+        public void ToPlainString()
+        {
+            // Arrange
+
+            var secureString = new SecureString();
+
+            secureString.AppendChar( 'a' );
+            secureString.AppendChar( 'h' );
+            secureString.AppendChar( '#' );
+
+            // Act
+
+            var plainText = secureString.ToPlainString();
+
+            // Assert
+
+            Assert.Equal( "ah#", plainText );
+        }
+
+        [Theory]
+        [InlineData( "ah#", "ah#", true )]
+        [InlineData( "88345^98?23", "88345^98!2", false )]
+        public void Comparison( string string1, string string2, bool expectedResult )
+        {
+            // Arrange
+
+            var secureString1 = CreateSecureString( string1 );
+            var secureString2 = CreateSecureString( string2 );
+
+            // Act
+            var result = secureString1.EqualTo( secureString2 );
+
+            // Assert
+            Assert.Equal( expectedResult, result );
+        }
+
+        private static SecureString CreateSecureString( string value )
+        {
+            var secureString = new SecureString();
+            foreach( var c in value )
+            {
+                secureString.AppendChar( c );
+            }
+            return secureString;
+        }
+    }
+}
---
+++ Lib/General/SecureStringUtilities.cs
@@ -0,0 +1,73 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2021-2025 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Utilities.DotNet
+{
+    /// <summary>
+    /// <see cref="SecureString"/> utilities.
+    /// </summary>
+    public static class SecureStringUtilities
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Gets the string representation of a <see cref="SecureString"/>.
+        /// </summary>
+        /// <param name="value">A secure string.</param>
+        /// <returns>The plain text representation of the secure string.</returns>
+        public static string ToPlainString( this SecureString value )
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            string? plainText;
+
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode( value );
+                plainText = Marshal.PtrToStringUni( valuePtr );
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode( valuePtr );
+            }
+
+            return CheckPlainText( plainText );
+        }
+
+        /// <summary>
+        /// Compares two <see cref="SecureString"/>.
+        /// </summary>
+        /// <param name="value">A secure string.</param>
+        /// <param name="other">Another secure string.</param>
+        /// <returns><see langword="true"/> if the current instance and <paramref name="other"/> have the same string representation;
+        ///          <see langword="false"/> otherwise.</returns>
+        public static bool EqualTo( this SecureString value, SecureString other )
+        {
+            var s1 = value.ToPlainString();
+            var s2 = other.ToPlainString();
+            return s1.Equals( s2 );
+        }
+
+        //===========================================================================
+        //                            PRIVATE METHODS
+        //===========================================================================
+
+        [ExcludeFromCodeCoverage]
+        private static string CheckPlainText( string? plainText )
+        {
+            if( plainText == null )
+            {
+                throw new InvalidOperationException( "Failed to convert the secure string to plain text." );
+            }
+
+            return plainText;
+        }
+    }
+}
