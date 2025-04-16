99efb7fe9a103214ee737d8c761b08ad15919f5f
Jesús González <gonzalez.jesus@safetwice.com>
2025-04-16 18:56:45 +0200

    Add ExceptionUtilities with methods to bypass exceptions

---
+++ Lib.Test/General/ExceptionUtilitiesTest.cs
@@ -0,0 +1,191 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2025 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using Xunit;
+using static Utilities.DotNet.ExceptionUtilities;
+
+namespace Utilities.DotNet.Test
+{
+    public class ExceptionUtilitiesTest
+    {
+        [Fact]
+        public void IgnoreExceptions_Action_NoException()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions( () =>
+            {
+                canary = true;
+            } );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.True( result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Action_All_Ignored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions( () =>
+            {
+                canary = true;
+
+                throw new InvalidOperationException();
+            } );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.False( result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Action_Specific_Ignored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions<InvalidOperationException>( () =>
+            {
+                canary = true;
+
+                throw new InvalidOperationException();
+            } );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.False( result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Action_Specific_NotIgnored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            Assert.Throws<InvalidOperationException>( () =>
+            {
+                BypassExceptions<ArgumentException>( () =>
+                {
+                    canary = true;
+
+                    throw new InvalidOperationException();
+                } );
+            } );
+
+            // Assert
+
+            Assert.True( canary );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Function_NoException()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions( () =>
+            {
+                canary = true;
+
+                return 0;
+            }, -1 );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.Equal( 0, result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Function_All_Ignored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions( () =>
+            {
+                canary = true;
+
+                throw new InvalidOperationException();
+            }, -1 );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.Equal( -1, result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Function_Specific_Ignored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            var result = BypassExceptions<InvalidOperationException, int>( () =>
+            {
+                canary = true;
+
+                throw new InvalidOperationException();
+            }, -33 );
+
+            // Assert
+
+            Assert.True( canary );
+            Assert.Equal( -33, result );
+        }
+
+        [Fact]
+        public void IgnoreExceptions_Function_Specific_NotIgnored()
+        {
+            // Arrange
+
+            bool canary = false;
+
+            // Act
+
+            Assert.Throws<InvalidOperationException>( () =>
+            {
+                BypassExceptions<ArgumentException, int>( () =>
+                {
+                    canary = true;
+
+                    throw new InvalidOperationException();
+                }, 333 );
+            } );
+
+            // Assert
+
+            Assert.True( canary );
+        }
+    }
+}
---
+++ Lib/General/ExceptionUtilities.cs
@@ -0,0 +1,80 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2025 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+
+namespace Utilities.DotNet
+{
+    /// <summary>
+    /// Exception utilities.
+    /// </summary>
+    public static class ExceptionUtilities
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Execute an action and ignore any exception that may be thrown.
+        /// </summary>
+        /// <param name="action">Action to execute.</param>
+        /// <returns><see langword="true"/> if the action executed successfully, <see langword="false"/> if an exception was thrown.</returns>
+        public static bool BypassExceptions( Action action )
+        {
+            return BypassExceptions<Exception>( action );
+        }
+
+        /// <summary>
+        /// Execute an action and ignore any exception of a given type that may be thrown.
+        /// </summary>
+        /// <typeparam name="TException">Type of exception to ignore.</typeparam>
+        /// <param name="action">Action to execute.</param>
+        /// <returns><see langword="true"/> if the action executed successfully, <see langword="false"/> if an exception was thrown.</returns>
+        public static bool BypassExceptions<TException>( Action action ) where TException : Exception
+        {
+            try
+            {
+                action();
+                return true;
+            }
+            catch( TException )
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Execute a function and ignore any exception that may be thrown.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the result.</typeparam>
+        /// <param name="function">Function to execute.</param>
+        /// <param name="fallbackValue">Fallback value to return in case of exception.</param>
+        /// <returns>Result of the function or the fallback value in case of exception.</returns>
+        public static TResult BypassExceptions<TResult>( Func<TResult> function, TResult fallbackValue )
+        {
+            return BypassExceptions<Exception, TResult>( function, fallbackValue );
+        }
+
+        /// <summary>
+        /// Execute a function and ignore any exception of a given type that may be thrown.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the result.</typeparam>
+        /// <typeparam name="TException">Type of exception to ignore.</typeparam>
+        /// <param name="function">Function to execute.</param>
+        /// <param name="fallbackValue">Fallback value to return in case of exception.</param>
+        /// <returns>Result of the function or the fallback value in case of exception.</returns>
+        public static TResult BypassExceptions<TException, TResult>( Func<TResult> function, TResult fallbackValue )
+             where TException : Exception
+        {
+            try
+            {
+                return function();
+            }
+            catch( TException )
+            {
+                return fallbackValue;
+            }
+        }
+    }
+}
