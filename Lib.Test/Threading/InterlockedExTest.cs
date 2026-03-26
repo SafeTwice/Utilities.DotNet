d9f9d376b1d2b07ff533a1f6e2129372e7443e66
Jesús González <gonzalez.jesus@safetwice.com>
2026-03-26 03:52:38 +0100

    Add methods for Interlocked-like operations with enums

---
+++ Lib.Test/Threading/InterlockedExTest.cs
@@ -0,0 +1,239 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2026 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+#if NET5_0_OR_GREATER
+
+using Xunit;
+
+namespace Utilities.DotNet.Threading.Test
+{
+
+    public class InterlockedExTest
+    {
+        public enum TestEnumInt { Zero = 0, One = 1, Two = 2, Three = 3 }
+        public enum TestEnumLong : long { Zero = 0, One = 1, Two = 2, Three = 3 }
+        public enum TestEnumByte : byte { Zero = 0, One = 1, Two = 2 }
+        public enum TestEnumShort : short { Zero = 0, One = 1, Two = 2 }
+
+        #region CompareExchange tests
+
+        [Theory]
+        [InlineData( TestEnumInt.Zero, TestEnumInt.Two, TestEnumInt.Zero, TestEnumInt.Two )]
+        [InlineData( TestEnumInt.One, TestEnumInt.Two, TestEnumInt.Zero, TestEnumInt.One )]
+        public void CompareExchange_IntEnum( TestEnumInt initialValue, TestEnumInt newValue, TestEnumInt comparedValue, TestEnumInt expectedFinal )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.CompareExchange( ref value, newValue, comparedValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( expectedFinal, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumLong.Zero, TestEnumLong.Two, TestEnumLong.Zero, TestEnumLong.Two )]
+        [InlineData( TestEnumLong.One, TestEnumLong.Two, TestEnumLong.Zero, TestEnumLong.One )]
+        public void CompareExchange_LongEnum( TestEnumLong initialValue, TestEnumLong newValue, TestEnumLong comparedValue, TestEnumLong expectedFinal )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.CompareExchange( ref value, newValue, comparedValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( expectedFinal, value );
+        }
+
+#if NET9_0_OR_GREATER
+
+        [Theory]
+        [InlineData( TestEnumByte.Zero, TestEnumByte.Two, TestEnumByte.Zero, TestEnumByte.Two )]
+        [InlineData( TestEnumByte.One, TestEnumByte.Two, TestEnumByte.Zero, TestEnumByte.One )]
+        public void CompareExchange_ByteEnum( TestEnumByte initialValue, TestEnumByte newValue, TestEnumByte comparedValue, TestEnumByte expectedFinal )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.CompareExchange( ref value, newValue, comparedValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( expectedFinal, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumShort.Zero, TestEnumShort.Two, TestEnumShort.Zero, TestEnumShort.Two )]
+        [InlineData( TestEnumShort.One, TestEnumShort.Two, TestEnumShort.Zero, TestEnumShort.One )]
+        public void CompareExchange_ShortEnum( TestEnumShort initialValue, TestEnumShort newValue, TestEnumShort comparedValue, TestEnumShort expectedFinal )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.CompareExchange( ref value, newValue, comparedValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( expectedFinal, value );
+        }
+
+#endif
+        #endregion
+
+        #region Exchange tests
+
+        [Theory]
+        [InlineData( TestEnumInt.Zero, TestEnumInt.Two )]
+        [InlineData( TestEnumInt.One, TestEnumInt.Three )]
+        public void Exchange_IntEnum( TestEnumInt initialValue, TestEnumInt newValue )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.Exchange( ref value, newValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( newValue, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumLong.Zero, TestEnumLong.Two )]
+        [InlineData( TestEnumLong.One, TestEnumLong.Three )]
+        public void Exchange_LongEnum( TestEnumLong initialValue, TestEnumLong newValue )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.Exchange( ref value, newValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( newValue, value );
+        }
+
+#if NET9_0_OR_GREATER
+
+        [Theory]
+        [InlineData( TestEnumByte.Zero, TestEnumByte.Two )]
+        [InlineData( TestEnumByte.One, TestEnumByte.Zero )]
+        public void Exchange_ByteEnum( TestEnumByte initialValue, TestEnumByte newValue )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.Exchange( ref value, newValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( newValue, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumShort.Zero, TestEnumShort.Two )]
+        [InlineData( TestEnumShort.One, TestEnumShort.Zero )]
+        public void Exchange_ShortEnum( TestEnumShort initialValue, TestEnumShort newValue )
+        {
+            // Arrange
+            var value = initialValue;
+
+            // Act
+            var original = InterlockedEx.Exchange( ref value, newValue );
+
+            // Assert
+            Assert.Equal( initialValue, original );
+            Assert.Equal( newValue, value );
+        }
+
+#endif
+        #endregion
+
+        #region And tests
+
+        [Theory]
+        [InlineData( TestEnumInt.Zero, TestEnumInt.Two, TestEnumInt.Zero )]
+        [InlineData( TestEnumInt.One, TestEnumInt.Two, TestEnumInt.Zero )]
+        [InlineData( TestEnumInt.Three, TestEnumInt.Two, TestEnumInt.Two )]
+        public void And_IntEnum( TestEnumInt firstValue, TestEnumInt secondValue, TestEnumInt finalValue )
+        {
+            // Arrange
+            var value = firstValue;
+
+            // Act
+            var original = InterlockedEx.And( ref value, secondValue );
+
+            // Assert
+            Assert.Equal( firstValue, original );
+            Assert.Equal( finalValue, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumLong.Zero, TestEnumLong.Two, TestEnumLong.Zero )]
+        [InlineData( TestEnumLong.One, TestEnumLong.Two, TestEnumLong.Zero )]
+        [InlineData( TestEnumLong.Three, TestEnumLong.Two, TestEnumLong.Two )]
+        public void And_LongEnum( TestEnumLong firstValue, TestEnumLong secondValue, TestEnumLong finalValue )
+        {
+            // Arrange
+            var value = firstValue;
+
+            // Act
+            var original = InterlockedEx.And( ref value, secondValue );
+
+            // Assert
+            Assert.Equal( firstValue, original );
+            Assert.Equal( finalValue, value );
+        }
+
+        #endregion
+
+        #region Or tests
+
+        [Theory]
+        [InlineData( TestEnumInt.Zero, TestEnumInt.Two, TestEnumInt.Two )]
+        [InlineData( TestEnumInt.One, TestEnumInt.Two, TestEnumInt.Three )]
+        [InlineData( TestEnumInt.Three, TestEnumInt.Two, TestEnumInt.Three )]
+        public void Or_IntEnum( TestEnumInt firstValue, TestEnumInt secondValue, TestEnumInt finalValue )
+        {
+            // Arrange
+            var value = firstValue;
+
+            // Act
+            var original = InterlockedEx.Or( ref value, secondValue );
+
+            // Assert
+            Assert.Equal( firstValue, original );
+            Assert.Equal( finalValue, value );
+        }
+
+        [Theory]
+        [InlineData( TestEnumLong.Zero, TestEnumLong.Two, TestEnumLong.Two )]
+        [InlineData( TestEnumLong.One, TestEnumLong.Two, TestEnumLong.Three )]
+        [InlineData( TestEnumLong.Three, TestEnumLong.Two, TestEnumLong.Three )]
+        public void Or_LongEnum( TestEnumLong firstValue, TestEnumLong secondValue, TestEnumLong finalValue )
+        {
+            // Arrange
+            var value = firstValue;
+
+            // Act
+            var original = InterlockedEx.Or( ref value, secondValue );
+
+            // Assert
+            Assert.Equal( firstValue, original );
+            Assert.Equal( finalValue, value );
+        }
+
+        #endregion
+    }
+}
+
+#endif
---
+++ Lib/Threading/InterlockedEx.cs
@@ -0,0 +1,207 @@
+﻿/// @file
+/// @license Public Domain
+
+// Based on https://stackoverflow.com/a/59127914/27871372
+
+#if NET5_0_OR_GREATER
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Utilities.DotNet.Threading
+{
+    /// <summary>
+    /// Utility class that provides atomic operations for enum types.
+    /// </summary>
+    public static class InterlockedEx
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Compares two instances of the specified enum <typeparamref name="TEnum"/> for equality and,
+        /// if they're equal, replaces the first value, as an atomic operation.
+        /// </summary>
+        /// <param name="location">The destination, whose value is compared with <paramref name="comparand"/> and possibly replaced.</param>
+        /// <param name="value">The value that replaces the <paramref name="location"/> value if the comparison results in equality.</param>
+        /// <param name="comparand">The value that is compared to the value at <paramref name="location"/>.</param>
+        /// <returns>The original value in <paramref name="location"/>.</returns>
+        public static TEnum CompareExchange<TEnum>( ref TEnum location, TEnum value, TEnum comparand )
+            where TEnum : struct, Enum
+        {
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+#if NET9_0_OR_GREATER
+                1 => CompareExchangeByte( ref location, value, comparand ),
+                2 => CompareExchangeShort( ref location, value, comparand ),
+#endif
+                4 => CompareExchangeInt( ref location, value, comparand ),
+                8 => CompareExchangeLong( ref location, value, comparand ),
+                _ => throw new NotSupportedException( "Unsupported enum size" )
+            };
+
+#if NET9_0_OR_GREATER
+            static TEnum CompareExchangeByte( ref TEnum location, TEnum value, TEnum comparand )
+            {
+                var comparandRaw = Unsafe.As<TEnum, byte>( ref comparand );
+                var valueRaw = Unsafe.As<TEnum, byte>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, byte>( ref location );
+                var returnRaw = Interlocked.CompareExchange( ref locationRaw, valueRaw, comparandRaw );
+                return Unsafe.As<byte, TEnum>( ref returnRaw );
+            }
+
+            static TEnum CompareExchangeShort( ref TEnum location, TEnum value, TEnum comparand )
+            {
+                var comparandRaw = Unsafe.As<TEnum, short>( ref comparand );
+                var valueRaw = Unsafe.As<TEnum, short>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, short>( ref location );
+                var returnRaw = Interlocked.CompareExchange( ref locationRaw, valueRaw, comparandRaw );
+                return Unsafe.As<short, TEnum>( ref returnRaw );
+            }
+#endif
+
+            static TEnum CompareExchangeInt( ref TEnum location, TEnum value, TEnum comparand )
+            {
+                int comparandRaw = Unsafe.As<TEnum, int>( ref comparand );
+                int valueRaw = Unsafe.As<TEnum, int>( ref value );
+                ref int locationRaw = ref Unsafe.As<TEnum, int>( ref location );
+                int returnRaw = Interlocked.CompareExchange( ref locationRaw, valueRaw, comparandRaw );
+                return Unsafe.As<int, TEnum>( ref returnRaw );
+            }
+
+            static TEnum CompareExchangeLong( ref TEnum location, TEnum value, TEnum comparand )
+            {
+                long comparandRaw = Unsafe.As<TEnum, long>( ref comparand );
+                long valueRaw = Unsafe.As<TEnum, long>( ref value );
+                ref long locationRaw = ref Unsafe.As<TEnum, long>( ref location );
+                long returnRaw = Interlocked.CompareExchange( ref locationRaw, valueRaw, comparandRaw );
+                return Unsafe.As<long, TEnum>( ref returnRaw );
+            }
+        }
+
+        /// <summary>
+        /// Sets an enum <typeparamref name="TEnum"/> to a specified value and returns the original value, as an atomic operation.
+        /// </summary>
+        /// <param name="location">The variable to set to the specified value.</param>
+        /// <param name="value">The value to which the <paramref name="location"/> parameter is set.</param>
+        /// <returns>The original value of <paramref name="location"/>.</returns>
+        public static TEnum Exchange<TEnum>( ref TEnum location, TEnum value )
+            where TEnum : struct, Enum
+        {
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+#if NET9_0_OR_GREATER
+                1 => ExchangeByte( ref location, value ),
+                2 => ExchangeShort( ref location, value ),
+#endif
+                4 => ExchangeInt( ref location, value ),
+                8 => ExchangeLong( ref location, value ),
+                _ => throw new NotSupportedException( "Unsupported enum size" )
+            };
+
+#if NET9_0_OR_GREATER
+            static TEnum ExchangeByte( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, byte>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, byte>( ref location );
+                var returnRaw = Interlocked.Exchange( ref locationRaw, valueRaw );
+                return Unsafe.As<byte, TEnum>( ref returnRaw );
+            }
+
+            static TEnum ExchangeShort( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, short>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, short>( ref location );
+                var returnRaw = Interlocked.Exchange( ref locationRaw, valueRaw );
+                return Unsafe.As<short, TEnum>( ref returnRaw );
+            }
+#endif
+
+            static TEnum ExchangeInt( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, int>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, int>( ref location );
+                var returnRaw = Interlocked.Exchange( ref locationRaw, valueRaw );
+                return Unsafe.As<int, TEnum>( ref returnRaw );
+            }
+
+            static TEnum ExchangeLong( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, long>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, long>( ref location );
+                var returnRaw = Interlocked.Exchange( ref locationRaw, valueRaw );
+                return Unsafe.As<long, TEnum>( ref returnRaw );
+            }
+        }
+
+        /// <summary>
+        /// Bitwise "ands" two enums <typeparamref name="TEnum"/> and replaces the first value with the result, as an atomic operation.
+        /// </summary>
+        /// <param name="location">A variable containing the first value to be combined and where the result is stored.</param>
+        /// <param name="value">The value to be combined with the enum at <paramref name="location"/>.</param>
+        /// <returns>The original value of <paramref name="location"/>.</returns>
+        public static TEnum And<TEnum>( ref TEnum location, TEnum value )
+            where TEnum : struct, Enum
+        {
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+                4 => AndInt( ref location, value ),
+                8 => AndLong( ref location, value ),
+                _ => throw new NotSupportedException( "Unsupported enum size" )
+            };
+
+            static TEnum AndInt( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, int>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, int>( ref location );
+                var returnRaw = Interlocked.And( ref locationRaw, valueRaw );
+                return Unsafe.As<int, TEnum>( ref returnRaw );
+            }
+
+            static TEnum AndLong( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, long>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, long>( ref location );
+                var returnRaw = Interlocked.And( ref locationRaw, valueRaw );
+                return Unsafe.As<long, TEnum>( ref returnRaw );
+            }
+        }
+
+        /// <summary>
+        /// Bitwise "ors" two enums <typeparamref name="TEnum"/> and replaces the first value with the result, as an atomic operation.
+        /// </summary>
+        /// <param name="location">A variable containing the first value to be combined and where the result is stored.</param>
+        /// <param name="value">The value to be combined with the enum at <paramref name="location"/>.</param>
+        /// <returns>The original value of <paramref name="location"/>.</returns>
+        public static TEnum Or<TEnum>( ref TEnum location, TEnum value )
+            where TEnum : struct, Enum
+        {
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+                4 => OrInt( ref location, value ),
+                8 => OrLong( ref location, value ),
+                _ => throw new NotSupportedException( "Unsupported enum size" )
+            };
+
+            static TEnum OrInt( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, int>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, int>( ref location );
+                var returnRaw = Interlocked.Or( ref locationRaw, valueRaw );
+                return Unsafe.As<int, TEnum>( ref returnRaw );
+            }
+
+            static TEnum OrLong( ref TEnum location, TEnum value )
+            {
+                var valueRaw = Unsafe.As<TEnum, long>( ref value );
+                ref var locationRaw = ref Unsafe.As<TEnum, long>( ref location );
+                var returnRaw = Interlocked.Or( ref locationRaw, valueRaw );
+                return Unsafe.As<long, TEnum>( ref returnRaw );
+            }
+        }
+    }
+}
+
+#endif
